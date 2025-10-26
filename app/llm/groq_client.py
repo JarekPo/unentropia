cdc98e2fe9c89e44e63c83ddaf8d0fe8ca9c53fe
Jarek Polowy <jarekpolowy@gmail.com>
2025-10-26 19:53:02 +0000

    update llm client

--- app/llm/groq_client.py
+++ app/llm/groq_client.py
@@ -1,18 +1,38 @@
 import os
-from groq import Groq
+
 from dotenv import load_dotenv
+from groq import Groq
 
 load_dotenv()
 
 
 class GroqClient:
-    def __init__(self, api_key: str = None, model: str = None):
+    def __init__(self, api_key: str = None, model: str = None, messages: list = None):
         self.api_key = api_key or os.getenv("GROQ_API_KEY")
         self.model = model or os.getenv("MODEL", "qwen/qwen3-32b")
         self.client = Groq(api_key=self.api_key)
+        self.messages = messages or []
 
     def get_response(self, user_message: str):
-        chat_completion = self.client.chat.completions.create(
-            messages=[{"role": "user", "content": user_message}], model=self.model
+        self.messages.append(
+            {
+                "role": "user",
+                "content": user_message,
+            }
         )
-        return chat_completion.choices[0].message.content
+        chat_completion = self.client.chat.completions.create(
+            messages=self.messages,
+            model=self.model,
+            reasoning_effort="none",  # "none" | "low" | "medium" | "high" - temporarly disabled
+        )
+        response = chat_completion.choices[0].message.content
+        self.messages.append(
+            {
+                "role": "assistant",
+                "content": response,
+            }
+        )
+        return response
+
+    def get_messages(self):
+        return self.messages
