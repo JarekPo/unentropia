bad721654255ee322c725fbe6f8d982f8a07f2a8
Jarek Polowy <jarekpolowy@gmail.com>
2025-10-30 21:31:06 +0000

    add CORS middleware

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ import uvicorn
 from api.v1.endpoints.chat import router as chat_router
 from api.v1.endpoints.health import router as health_router
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Unentropia API", version="0.0.1")
 
@@ -9,6 +10,17 @@ app = FastAPI(title="Unentropia API", version="0.0.1")
 app.include_router(health_router, prefix="/api/v1/health", tags=["health"])
 app.include_router(chat_router, prefix="/api/v1/chat", tags=["chat"])
 
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST"],
+    allow_headers=["*"],
+)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
