ef76191b867c81af1407722fc982b68c025afe3c
Jarek Polowy <jarekpolowy@gmail.com>
2026-02-19 07:42:37 +0000

    remove wildcard origins

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from api.v1.endpoints.chat import router as chat_router
-from api.v1.endpoints.health import router as health_router
-from auth.routes import router as auth_router
 from fastapi import FastAPI, Request, status
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from jose import exceptions
+
+from api.v1.endpoints.chat import router as chat_router
+from api.v1.endpoints.health import router as health_router
+from auth.routes import router as auth_router
 
 app = FastAPI(title="Unentropia API", version="0.0.1")
 
@@ -13,7 +14,7 @@ app.include_router(health_router, prefix="/api/v1/health", tags=["health"])
 app.include_router(chat_router, prefix="/api/v1/chat", tags=["chat"])
 app.include_router(auth_router, prefix="/auth/google", tags=["auth"])
 
-origins = ["http://localhost:3000", "*"]  # Allow all origins for development; adjust in production
+origins = ["http://localhost:3000"]
 
 app.add_middleware(
     CORSMiddleware,
